1313f26f67740a5abeabe8d06f079aafc83d59f6
pai-puella <cat.dyshkant@yandex.ru>
2023-09-20 21:47:06 +0300

    Added Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-// import
+import java.util.Scanner;
 
 public class Main {
+    public static void main (String[] args) {
+        System.out.println("Задача 1");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите 0, если у вас IPhone, или 1, если Android: ");
+        int clientOS = scanner.nextInt();
+
+        switch (clientOS) {
+            case 0 -> System.out.println("Установите версию приложения для iOS по ссылке");
+            case 1 -> System.out.println("Установите версию приложения для Android по ссылке");
+            default -> System.out.println("Некорректное значение переменной clientOS");
+        }
+    }
 
 }
