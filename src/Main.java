68da0b2cd024c024215f59d5d82d3ed3d7290a58
pai-puella <cat.dyshkant@yandex.ru>
2023-09-20 23:22:16 +0300

    Added Task 2

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,30 @@ public class Main {
             case 1 -> System.out.println("Установите версию приложения для Android по ссылке");
             default -> System.out.println("Некорректное значение переменной clientOS");
         }
+
+        System.out.println();
+        System.out.println("Задача 2");
+        System.out.print("Введите год создания телефона: ");
+        int clientDeviceYear = scanner.nextInt();
+
+        switch (clientOS) {
+            case 0 -> {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+            }
+            case 1 -> {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+            }
+            default -> System.out.println("Некорректное значение переменной clientOS");
+        }
+
     }
 
 }
